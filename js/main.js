00508a1259260c4654efeb496af8db85a3136b81
Mahmoud Younis <mahmoudyounis5761@gmail.com>
2024-07-09 03:31:24 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,9 +2,13 @@ const head_text = document.querySelector("#head_text")
 const link__ = document.querySelector("#link")
 const message = document.querySelector("#note")
 const element = document.querySelector("#item")
+const btn_insrt = document.querySelector("#btn_insrt")
+const edit_btn = document.querySelector("#edit_btn")
 const allBooked = []
 
-function addNewLink() {
+
+
+btn_insrt.addEventListener('click', () => {
     var newBookedLink = {};
     if (head_text.value == "") {
         head_text.placeholder = "pls enter valid data...";
@@ -19,7 +23,12 @@ function addNewLink() {
         displayAll();
         clear();
     }
-}
+    if (btn_insrt.innerHTML == "Edit") {
+        btn_insrt.innerHTML = "Add";
+        clear();
+    }
+
+})
 
 function displayAll() {
     var storage = "";
@@ -27,12 +36,10 @@ function displayAll() {
         storage += `    <div class="col-md-3 mb-4 text-center " >   
                 <div class="p-2 border border-2 rounded-2">
                     <h2 class="mb-2">${allBooked[i].head}</h2>
-                   
                     <p>${allBooked[i].message}</p>
-                 
-                   <div class="d-flex justify-content-between px-2">
+                    <div class="d-flex justify-content-between px-2">
                     <a href="${allBooked[i].link}" target="_blank"><i class="fa-solid fa-arrow-up-right-from-square "></i></a>
-                    <i onclick="edit(${i})" class="fa-regular fa-pen-to-square"></i>
+                    <i onclick="edit(${i})" id="edit_btn" class="fa-regular fa-pen-to-square"></i>
                     <i onclick="deleteitem(${i})" class="fa-solid fa-trash text-danger"></i> </div>
                 </div>
             </div>`
@@ -48,4 +55,16 @@ function clear() {
 function deleteitem(indx) {
     allBooked.splice(indx, 1);
     displayAll();
-}
+}   
+function edit(indx) {
+        btn_insrt.innerHTML = "Edit"
+        head_text.value = allBooked[indx].head;
+        link__.value = allBooked[indx].link;
+        message.value = allBooked[indx].message;
+        btn_insrt.addEventListener('click', () => {
+            allBooked[indx].head = head_text.value;
+            allBooked[indx].link = link__.value;
+            allBooked[indx].message=message.value;
+            deleteitem(indx)
+        })
+    }
